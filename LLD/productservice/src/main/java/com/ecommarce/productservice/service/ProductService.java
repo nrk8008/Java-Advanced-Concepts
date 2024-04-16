1586f2c921adf82f25fbaf294c086daae6e15177
nrk8008 <nrk8008@gmail.com>
2024-04-16 08:46:54 +0530

    productService added

--- LLD/productservice/src/main/java/com/ecommarce/productservice/service/ProductService.java
+++ LLD/productservice/src/main/java/com/ecommarce/productservice/service/ProductService.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
      public GetProductDto getProductById(Long id){
-        // call the DB
-         // call the fake store API
+        /*
+          call the DB
+          call the fake store API
+         */
+
          GetProductDto obj = new GetProductDto();
          obj.setName("I PHONE");
          obj.setPrice(120000.0);
